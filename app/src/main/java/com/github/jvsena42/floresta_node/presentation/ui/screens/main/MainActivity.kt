56b0e66239507c6817aa2fb018bc80a9313c1162
Joao Victor Sena <jvsena16@gmail.com>
2025-02-26 12:45:05 -0300

    refactor: change Theme composable name

--- app/src/main/java/com/github/jvsena42/floresta_node/presentation/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/github/jvsena42/floresta_node/presentation/ui/screens/main/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.jvsena42.floresta_node.presentation.ui.theme.Floresta_nodeTheme
+import com.github.jvsena42.floresta_node.presentation.ui.theme.FlorestaNodeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Floresta_nodeTheme {
+            FlorestaNodeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Floresta_nodeTheme {
+    FlorestaNodeTheme {
         Greeting("Android")
     }
 }
