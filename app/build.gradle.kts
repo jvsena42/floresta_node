38a73aee35e2a5092bf553d5c372711ba169d4a6
Joao Victor Sena <jvsena16@gmail.com>
2025-02-26 12:35:59 -0300

    chore: navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.compose.navigation)
 
     implementation(project.dependencies.platform(libs.koin.bom))
     implementation(libs.koin.core)
